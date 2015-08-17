a1993b02c27797a6d423b40be455286c09b659f2
kbarabash <barkir89@gmail.com>
2015-08-17 13:53:17 +0300

    add build

---
+++ .webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loaders: 'babel',
+            exclude: /(node_modules|bower_components)/
+        }]
+    },
+    entry: {
+        app: ['src/index.js']
+    },
+    output: {
+        path: "./build",
+        publicPath: "/assets/",
+        filename: "bundle.js"
+    }
+    resolve: {
+        extensions: ['', '.js']
+    }
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,39 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var jasmine = require('gulp-jasmine');
 var reporters = require('jasmine-reporters');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var webpackConfig = require('.webpack.config.js');
 
-gulp.task('test', function () {
+gulp.task('test', function() {
     return gulp.src('tests/test.js')
         .pipe(jasmine({
             reporter: new reporters.JUnitXmlReporter()
         }));
+});
+
+gulp.task('build', function(callback) {
+    webpack(webpackConfig, function (err, stats) {
+        if (err) {
+            throw new gutil.PluginError('webpack', err);
+        }
+        gutil.log('[build]', stats.toString({
+            // output options
+        }));
+        callback();
+    });
+});
+
+gulp.task('serve', function(callback) {
+    var compiler = webpack(webpackConfig);
+
+    new WebpackDevServer(compiler, {
+    }).listen(8080, 'localhost', function (err) {
+        if (err) {
+            throw new gutil.PluginError('webpack-dev-server', err);
+        }
+        gutil.log('[serve]', 'http://localhost:8080/webpack-dev-server/index.html');
+        callback();
+    });
 });
