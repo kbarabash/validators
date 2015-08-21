c13b79cd87d76b8cd6f73455c60c9f04333cfd70
Kirill Barabash <barkir89@gmail.com>
2015-08-21 14:13:21 +0300

    add tests

--- src/validators/items/email.js
+++ src/validators/items/email.js
@@ -1,12 +1,14 @@
 import AbstractValidator from '../core/AbstractValidator';
 
+const EMAIL_REGEXP = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/i;
+const NAME = 'EmailValidator';
+
 export default class Email extends AbstractValidator {
     constructor() {
         super();
-        this.name = 'EmailValidator';
+        this.name = NAME;
     }
     isValid(email) {
-        var emailRegexp = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/i;
-        return emailRegexp.test(email);
+        return EMAIL_REGEXP.test(email);
     }
 };
