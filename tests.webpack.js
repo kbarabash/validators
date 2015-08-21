e918809173afa37f571e6c1a2a69d32ebe53c3cd
Kirill Barabash <barkir89@gmail.com>
2015-08-21 14:07:41 +0300

    remove tests folder

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,7 +1,7 @@
 var Immutable = require('immutable');
 require('core-js/es5');
 
-var projectContext = require.context('../src', true, /\.test\.js$/);
+var projectContext = require.context('./src', true, /\.test\.js$/);
 var projectModuleIds = Immutable.Set(
     projectContext.keys().map(module => String(projectContext.resolve(module)))
 );
@@ -11,5 +11,5 @@ beforeEach(() => {
     projectModuleIds.forEach(id => delete cache[id]);
 });
 
-var context = require.context('../src', true, /\.test\.js$/);
+var context = require.context('./src', true, /\.test\.js$/);
 context.keys().forEach(context);
